417f738a0a232373aa440ac495a31b2164c04d21
Rachit Handa <rachithanda93@yahoo.com>
2022-02-26 15:29:06 +0530

    changes for stacks and queues

---
+++ data-structures/stacks_custom_method.js
@@ -0,0 +1,10 @@
+/**
+ * Stacks are LIFO data structure, which means the last element added to the stack 
+ * will be the first element to leave the stack.
+ * 
+ * <--------Use Case--------->
+ * managing functions invocations
+ * undo and redo operations
+ * Routing (history Object) is treated like a stack
+ * 
+ */
