e00363cd38d28ff3de246f8577493162c647136f
Rachit Handa <rachithanda93@yahoo.com>
2022-03-03 00:35:01 +0530

    changes for binary heap

---
+++ data-structures/Binaryheaps.js
@@ -0,0 +1,6 @@
+/** Its very similar to a binary tree with but set of rules
+ * 
+ * In MaxBinaryHeap ,parent nodes are always larger than the child nodes.
+ * In MinBinaryHeap, parent nodes are always smaller than child nodes.
+ * 
+ */
