99bd3767f7329cb729329d54db3f0a53aeb2aeaa
rhanda92 <rachit.handa@amadeus.com>
2022-12-01 15:31:40 +0530

    solution for letter replacement substring

--- Grokking_Coding_patterns/Sliding_window/9.Longest_Substring_with_Same_Letters _after_Replacement _(hard).js
+++ Grokking_Coding_patterns/Sliding_window/9.Longest_Substring_with_Same_Letters _after_Replacement _(hard).js
@@ -8,11 +8,13 @@ Example 1:
 Input: String="aabccbb", k=2
 Output: 5
 Explanation: Replace the two 'c' with 'b' to have a longest repeating substring "bbbbb".
+
 Example 2:
 
 Input: String="abbcb", k=1
 Output: 4
 Explanation: Replace the 'c' with 'b' to have a longest repeating substring "bbbb".
+
 Example 3:
 
 Input: String="abccde", k=1
@@ -21,12 +23,36 @@ Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating subst
 */
 
 function length_of_longest_substring(str, k) {
+    let windowStart = 0;
+    let windowEnd;
+    let maxCharCount = 0;
+    let charMap = {};
+    let maxLength = 0;
+    // Try to extend the range [windowStart, windowEnd]
+    for (windowEnd = 0; windowEnd < str.length; windowEnd++) {
+        let rightChar = str[windowEnd];
+        charMap[rightChar] = (charMap[rightChar] || 0) + 1;
+        maxCharCount = charMap[rightChar];
 
+        // Current window size is from windowStart to windowEnd, overall we have a letter which is
+        // repeating 'maxRepeatLetterCount' times, this means we can have a window which has one letter
+        // repeating 'maxRepeatLetterCount' times and the remaining letters we should replace.
+        // if the remaining letters are more than 'k', it is the time to shrink the window as we
+        // are not allowed to replace more than 'k' letters.
+
+        if (windowEnd - windowStart + 1 - maxCharCount > k) {
+            let leftChar = str[windowStart];
+            charMap[leftChar] -= 1;
+            windowStart += 1;
+        }
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
 }
 
-console.log(length_of_longest_substring('aabccbb', 2));
-console.log(length_of_longest_substring('abbcb', 1));
-console.log(length_of_longest_substring('abccde', 1));
+console.log(`length of longest substring with same letters after 2 character replacement: ${length_of_longest_substring('aabccbb', 2)}`);
+console.log(`length of longest substring with same letters after 1 character replacement: ${length_of_longest_substring('abbcb', 1)}`);
+console.log(`length of longest substring with same letters after 1 character replacement: ${length_of_longest_substring('abccde', 1)}`);
 
 /*
 Time Complexity #
