4319b47eb7dbe7c8d4654e4a7094ee4b0cf261aa
rhanda92 <rachit.handa@amadeus.com>
2022-12-27 13:11:06 +0530

    changes to longest subarray

--- Grokking_Coding_patterns/Sliding_window/7.Longest_Subarray_with_Ones_after_Replacement__hard.js
+++ Grokking_Coding_patterns/Sliding_window/7.Longest_Subarray_with_Ones_after_Replacement__hard.js
@@ -20,12 +20,28 @@ This problem follows the Sliding Window pattern and is quite similar to Longest 
 Following a similar approach, we’ll iterate through the array to add one number at a time in the window. We’ll also keep track of the maximum number of repeating 1s in the current window (let’s call it maxOnesCount). So at any time, we know that we can have a window which has 1s repeating maxOnesCount time, so we should try to replace the remaining 0s. If we have more than ‘k’ remaining 0s, we should shrink the window as we are not allowed to replace more than ‘k’ 0s.
 */
 
-function length_of_longest_substring(arr, k){
-
+function length_of_longest_substring(arr, k) {
+    let windowStart = 0;
+    let windowEnd;
+    let maxOnesCount = 0;
+    let maxLength = 0;
+    for (windowEnd=0;windowEnd<arr.length;windowEnd++){
+        if(arr[windowEnd]===1){
+            maxOnesCount+=1;
+        }
+        if(windowEnd-Start-maxOnesCount+1>k){
+            if(arr[windowStart===1]){
+                maxOnesCount-=1;
+            }
+            windowStart+=1;
+        }
+        maxLength=Math.max(maxLength,windowEnd-windowStart+1);
+    }
+    return maxLength;
 }
 
-console.log(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2));
-console.log(length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3));
+console.log(`length of the longest contiguous subarray having all 1s: ${length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2)}`);
+console.log(`length of the longest contiguous subarray having all 1s: ${length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3)}`);
 
 /*
 Time Complexity #
